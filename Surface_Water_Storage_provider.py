bba61f7fa0939ac134825de9fb16913d042a1e35
João Vitor Pimenta <jvpjoaopimenta@gmail.com>
2024-11-24 19:05:41 -0300

    refactor: change the order of imports

--- Surface_Water_Storage_provider.py
+++ Surface_Water_Storage_provider.py
@@ -30,11 +30,12 @@ __copyright__ = '(C) 2024 by João Vitor Pimenta'
 
 __revision__ = '$Format:%H$'
 
+import os
 from qgis.core import QgsProcessingProvider
 from qgis.PyQt.QtGui import QIcon
 from .area_height_volume_graph_algorithm import createAreaHeightVolumeGraphAlgorithm
 from .flooded_area_algorithm import createFloodedAreaAlgorithm
-import os
+
 
 
 class SurfaceWaterStorageProvider(QgsProcessingProvider):
